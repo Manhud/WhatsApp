2fc8edc56a84775c174598daf1bd8aef0d01672c
Juan Guillermo Rozo <rozoacwow@gmail.com>
2020-09-04 20:27:43 -0500

    sidebar component

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAxDvOcPWKHbuBu9EjHSeoNXNoqK4z83JU",
+    authDomain: "whatsapp-mongo.firebaseapp.com",
+    databaseURL: "https://whatsapp-mongo.firebaseio.com",
+    projectId: "whatsapp-mongo",
+    storageBucket: "whatsapp-mongo.appspot.com",
+    messagingSenderId: "900001603873",
+    appId: "1:900001603873:web:6207b455e2cb0b480ede60"
+  };
