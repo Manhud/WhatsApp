ef702097b8e07b13d1b69a6fe5c5f3f9c98cf9df
Juan Guillermo Rozo <rozoacwow@gmail.com>
2020-09-21 19:07:26 -0500

    [Whatsapp-frontend] v1.0

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import "./Sidebar.css";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge"
+import MoreVertIcon from "@material-ui/icons/MoreVert"
+import ChatIcon from "@material-ui/icons/Chat"
+import { SearchOutlined } from "@material-ui/icons"
+import {IconButton, Avatar} from '@material-ui/core'
+import SidebarChat from './SidebarChat';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-        I am the sidebar
+            <div className="sidebar__header">
+            <Avatar src="https://web.whatsapp.com/pp?e=https%3A%2F%2Fpps.whatsapp.net%2Fv%2Ft61.24694-24%2F95075730_1338756326317680_1697712689075254866_n.jpg%3Foh%3D950b0068cf2eabdbd90b934d770528fa%26oe%3D5F57C4EC&t=s&u=573232205135%40c.us&i=1590586962&n=dsLy6R%2FjmRKxclF9pGryTynp2YDHXygWHT45f9bijUc%3D"/>
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined/>
+                    <input placeholder="Search or start new chat" type="text"></input>
+                </div>
+            </div>
+            <div className="sidebar__chats">
+                <SidebarChat/>
+                <SidebarChat/>
+                <SidebarChat/>
+            </div>
         </div>
     )
 }
